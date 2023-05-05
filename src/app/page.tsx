4a1c002ada48bca508c8e10ea5506c16f97d08b8
Logan9312 <lgntravis@gmail.com>
2023-05-05 11:53:14 -0600

    Adding text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import Logo from '../../components/Logo';
 
 const Home = () => {
   return (
-    <div style={{ color: 'black' , backgroundColor: 'white' }}>
+    <div style={{ color: 'black', backgroundColor: 'white' }}>
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <img src="/logo.png" alt="Company Logo" style={{ width: '100px', marginRight: '20px' }} />
         <Navbar />
       </div>
-      <h1>Welcome to my website!</h1>
+      <h1>Travis Drilling</h1>
       <p>Water well drilling and maintenance</p>
     </div>
   );
