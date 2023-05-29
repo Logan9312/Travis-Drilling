caa87a022161bdfff5ae0809b3de8162e2a9c7fb
Logan <lgntravis@gmail.com>
2023-05-28 19:25:24 -0600

    restoring files

---
+++ components/infobar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BottomInfoBar() {
+  return (
+    <div style={{ backgroundColor: '#00bfff', padding: '10px' }}>
+      <p>This is a static message for the bottom info bar.</p>
+    </div>
+  );
+}
+
+export default BottomInfoBar;
