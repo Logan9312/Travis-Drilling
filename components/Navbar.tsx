531debec8f48b892d50d8918b2dc554e347d65ab
Logan <lgntravis@gmail.com>
2023-05-28 19:50:37 -0600

    Trying next image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,16 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar: React.FC = () => {
   return (
 
     <div style={{ display: 'flex', alignItems: 'center', padding: '20px' }}>
-      <img src="logo.png" alt="Company Logo" style={{ width: '100px', marginRight: '20px' }} />
-
-
+      <Image
+        src="/logo.png"
+        alt="Travis Drilling Logo"
+        width={100}
+        height={100}
+      />
       <nav style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <ul style={{ listStyle: 'none', display: 'flex', color: 'black' }}>
           <NavItem href="/">Home</NavItem>
