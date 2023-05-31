e7dbd0e28b992a9e25350e0eaed9bdd15eca7349
Logan <lgntravis@gmail.com>
2023-05-30 19:58:44 -0600

    closing image tag

--- src/pages/new.tsx
+++ src/pages/new.tsx
@@ -150,7 +150,7 @@ const Services = () => <section className="pb-20 bg-gray-300 -mt-24">
             alt="..."
             src={"https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"}
             className="w-full align-middle rounded-t-lg"
-          >
+          ></Image>
             <blockquote className="relative p-8 mb-4">
               <svg
                 preserveAspectRatio="none"
