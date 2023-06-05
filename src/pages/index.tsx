e5b13b952816910cf56d2e87490b32bf9ecde899
Logan <lgntravis@gmail.com>
2023-06-05 11:18:13 -0600

    removing useless formatting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -180,7 +180,7 @@ const Services = () => <section className="pb-20 bg-gray-300 -mt-24">
   </div>
 </section>
 
-const Featured = () => <section className="relative py-20">
+const Featured = () => <section className="relative py-10">
   <div
     className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20"
     style={{ height: "80px" }}
@@ -200,78 +200,9 @@ const Featured = () => <section className="relative py-20">
       ></polygon>
     </svg>
   </div>
-
-  <div className="container mx-auto px-4">
-    <div className="items-center flex flex-wrap">
-      <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
-        <Image
-          width="100"
-          height="100"
-          alt="..."
-          className="max-w-full rounded-lg shadow-lg"
-          src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-        />
-      </div>
-      <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
-        <div className="md:pr-12">
-          <div className="text-pink-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-pink-300">
-            <i className="fas fa-rocket text-xl"></i>
-          </div>
-          <h3 className="text-3xl font-semibold">
-            A growing company
-          </h3>
-          <p className="mt-4 text-lg leading-relaxed text-gray-600">
-            The extension comes with three pre-built pages to help you
-            get started faster. You can change the text and images and
-            you&apos;re good to go.
-          </p>
-          <ul className="list-none mt-6">
-            <li className="py-2">
-              <div className="flex items-center">
-                <div>
-                  <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
-                    <i className="fas fa-fingerprint"></i>
-                  </span>
-                </div>
-                <div>
-                  <h4 className="text-gray-600">
-                    Carefully crafted components
-                  </h4>
-                </div>
-              </div>
-            </li>
-            <li className="py-2">
-              <div className="flex items-center">
-                <div>
-                  <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
-                    <i className="fab fa-html5"></i>
-                  </span>
-                </div>
-                <div>
-                  <h4 className="text-gray-600">Amazing page examples</h4>
-                </div>
-              </div>
-            </li>
-            <li className="py-2">
-              <div className="flex items-center">
-                <div>
-                  <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
-                    <i className="far fa-paper-plane"></i>
-                  </span>
-                </div>
-                <div>
-                  <h4 className="text-gray-600">Dynamic components</h4>
-                </div>
-              </div>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
 </section>
 
-const Team = () => <section className="pt-20 pb-48">
+const Team = () => <section className="pt-10 pb-48">
   <div className="container mx-auto px-4">
     <div className="flex flex-wrap justify-center text-center mb-24">
       <div className="w-full lg:w-6/12 px-4">
