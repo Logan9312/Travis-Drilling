b756197a059c3cef8cb040a8ebd3394f2a5c876b
Logan <lgntravis@gmail.com>
2023-06-05 21:23:00 -0600

    Resources Page

---
+++ src/pages/resources.tsx
@@ -0,0 +1,17 @@
+import Navbar from "../components/Navbar";
+
+const Resources: React.FC = () => {
+    return (
+        <div className="bg-white min-h-screen flex flex-col">
+            <Navbar />
+            <div className="flex-1 flex flex-col justify-center items-center">
+                <div className="mt-8 text-center">
+                    Page Under Construction. Business Resources will be added here.
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Resources;
