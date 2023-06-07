e27ce9071bf00ead48800dd6d3f1d4acfbb796a0
Logan <lgntravis@gmail.com>
2023-06-07 16:33:58 -0600

    Favicon

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -19,7 +19,7 @@ const Layout = (props: PropsWithChildren<Props>) => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta lang="en" />
-        <link href="/logo.png" rel="icon" />
+        <link href="/favicon.ico" rel="icon" />
       </Head>
       <Script src="https://kit.fontawesome.com/e6ae3fbb74.js" crossOrigin="anonymous"></Script>
       <header>
