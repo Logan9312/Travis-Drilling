949086284a1c61bee11ce22779764c605dc72b5d
Logan <lgntravis@gmail.com>
2023-06-07 16:47:46 -0600

    Facivons

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -19,7 +19,13 @@ const Layout = (props: PropsWithChildren<Props>) => {
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta lang="en" />
-        <link href="/favicon.ico" rel="icon" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+        <meta name="msapplication-TileColor" content="#ff0000" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <Script src="https://kit.fontawesome.com/e6ae3fbb74.js" crossOrigin="anonymous"></Script>
       <header>
