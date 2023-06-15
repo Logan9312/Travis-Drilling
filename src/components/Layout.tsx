ceaf018a43981d5a9b4b81d8fe2530186cd9d190
Logan <lgntravis@gmail.com>
2023-06-15 09:27:36 -0600

    Testing Favicon Change

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -25,7 +25,7 @@ const Layout = (props: PropsWithChildren<Props>) => {
         />
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="shortcut icon" href="/favicon.ico" />
-        <link
+        {/*<link
           rel="icon"
           type="image/png"
           sizes="32x32"
@@ -36,7 +36,7 @@ const Layout = (props: PropsWithChildren<Props>) => {
           type="image/png"
           sizes="16x16"
           href="/favicon-16x16.png"
-        />
+        />*/}
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
         <meta name="msapplication-TileColor" content="#ff0000" />
