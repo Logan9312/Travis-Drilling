64c887681a4dbf083fc81160d3e9b5755c07f45e
Logan Travis <lgntravis@gmail.com>
2026-06-11 10:41:24 -0600

    Vite config fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		// esbuild can't scan .svelte files; lucide-svelte recommends excluding it
+		exclude: ['lucide-svelte']
+	}
 });
